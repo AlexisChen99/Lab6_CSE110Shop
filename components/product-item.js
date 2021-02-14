ff464bacad81cd5af643586519cec66092c2bab1
AlexisChen99 <anc009@ucsd.edu>
2021-02-13 20:19:19 -0800

    finish the assignment

--- components/product-item.js
+++ components/product-item.js
@@ -1,7 +1,142 @@
-// product-item.js
-
 class ProductItem extends HTMLElement {
-  // TODO
+  constructor(id, imgSrc, title, itemPrice, added) {
+    //always call super first in the constructor
+    super();
+
+    // create a shadowroot that attach to product-list
+    const shadowRoot = this.attachShadow({ mode: "open" });
+
+    //create the name of the product
+    var name = document.createElement("p");
+    name.setAttribute("class", "title");
+    name.innerHTML = title;
+
+    //creat the image of the product
+    var image = document.createElement("img");
+    image.setAttribute("width", 200);
+    image.setAttribute("alt", title);
+    image.setAttribute("src", imgSrc);
+
+    //creat the price of the product
+    var price = document.createElement("p");
+    price.setAttribute("class", "price");
+    price.innerHTML = "$" + itemPrice;
+
+    //create the button of the product 
+    var button = document.createElement("button");
+    button.innerHTML = added;
+
+
+    //when users click, change added and modify cart and count
+    button.onclick = function () {
+      //get the count element
+      let count = document.getElementById("cart-count");
+
+      //if the item is not added 
+      if (button.innerHTML == "Add to Cart") {
+        //add the item and change the added to remove
+        addList.push(id);
+        button.innerHTML = "Remove from Cart";
+        alert("Added to Cart!!");
+      }
+      //if the item is also added
+      else {
+        //remove the item from the cart 
+        addList.splice(addList.indexOf(id), 1);
+        button.innerHTML = "Add to Cart";
+        alert("Removed from Cart!");
+      }
+
+      //update cart in local storage and count in the page
+      locStor.setItem("itemCount", JSON.stringify(addList));
+      count.innerHTML = addList.length;
+    };
+
+    // create a list that stores name, image, price, and button
+    var list = document.createElement("li");
+    list.setAttribute("class", "product");
+
+    // Store the data onto the 'li' tag
+    list.appendChild(image);
+    list.appendChild(name);
+    list.appendChild(price);
+    list.appendChild(button);
+
+    //create the element style 
+    var style = document.createElement("style");
+
+    //append list and style to the root 
+    shadowRoot.appendChild(list);
+    shadowRoot.appendChild(style);
+
+
+    //css style
+    style.textContent = `
+    .price {
+      color: green;
+      font-size: 1.8em;
+      font-weight: bold;
+      margin: 0;
+    }
+    
+    .product {
+      align-items: center;
+      background-color: white;
+      border-radius: 5px;
+      display: grid;
+      grid-template-areas: 
+      'image'
+      'title'
+      'price'
+      'add';
+      grid-template-rows: 67% 11% 11% 11%;
+      height: 450px;
+      filter: drop-shadow(0px 0px 6px rgb(0,0,0,0.2));
+      margin: 0 30px 30px 0;
+      padding: 10px 20px;
+      width: 200px;
+    }
+    
+    .product > button {
+      background-color: rgb(255, 208, 0);
+      border: none;
+      border-radius: 5px;
+      color: black;
+      justify-self: center;
+      max-height: 35px;
+      padding: 8px 20px;
+      transition: 0.1s ease all;
+    }
+    
+    .product > button:hover {
+      background-color: rgb(255, 166, 0);
+      cursor: pointer;
+      transition: 0.1s ease all;
+    }
+    
+    .product > img {
+      align-self: center;
+      justify-self: center;
+      width: 100%;
+    }
+    
+    .title {
+      font-size: 1.1em;
+      margin: 0;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
+    
+    .title:hover {
+      font-size: 1.1em;
+      margin: 0;
+      white-space: wrap;
+      overflow: auto;
+      text-overflow: unset;
+    }
+    `;
+  }
 }
 
-customElements.define('product-item', ProductItem);
+customElements.define("product-item", ProductItem);
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,65 @@
-// Script.js
+var locStor = window.localStorage;
+var addList = [];
 
-window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+window.addEventListener("DOMContentLoaded", () => {
+  if (locStor.getItem("items") == null) {
+    pullFetch();
+    createAndPutItems();
+  }
+  createAndPutItems()
 });
+
+/**
+ * fetch from the url and store the data in the local storage
+ */
+async function pullFetch() {
+  const response = await fetch("https://fakestoreapi.com/products");
+  const json = await response.json();
+  locStor.setItem("store", JSON.stringify(json));
+}
+
+/**
+ * creates items and put them in the store 
+ */
+async function createAndPutItems() {
+  // get the items in the store 
+  var stores= JSON.parse(locStor.getItem("store"));
+  var productList = document.getElementById("product-list");
+
+  // if cart already exists
+  if (locStor.getItem("itemCount")) {
+    // get the cart from html and local storage and update  
+    var count = document.getElementById("cart-count");
+    addList = JSON.parse(locStor.getItem("itemCount"));
+    count.innerHTML = addList.length;
+    //if the card doesn't exist in local storage
+  } else {
+    // add an empty list to cart
+    locStor.setItem("itemCount", addList);
+  }
+
+  //for all items, create an element for each of them and put it on the page
+  for (i in stores) {
+    var proItem;
+
+    //id, imgSrc, title, itemPrice, added
+    let id = stores[i].id;
+    let imgSrc = stores[i].image;
+    let title = stores[i].title;
+    let price = stores[i].price;
+    let added;
+
+    //if the list has current item
+    if (addList.includes(stores[i].id)) {
+      added = "Remove from Cart";
+    //if the list doesn't have current item 
+    } else {
+      added =  "Add to Cart";
+    }
+    //create the item element
+    proItem = new ProductItem(id, imgSrc, title, price, added);
+
+    //add the created item to product-list
+    productList.appendChild(proItem);
+  }
+}
